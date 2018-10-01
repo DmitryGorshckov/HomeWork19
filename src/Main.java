864e47d215cd3c7388cab6e51c7a35965886db2c
dmitry <what-do-you-wanttt@yandex.ru>
2018-10-01 12:51:38 +0300

    Д/з 19

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(new FileReader("products.txt"))) {
-            System.out.printf("%-14s%-6s%-12s%-10s%n", "Наименование", "Цена", "Количество", "Стоимость");
+            System.out.printf("%-14s%-6s%-12s%-10s%-5s%n", "Наименование", "Цена", "Количество", "Стоимость","Итого");
             System.out.println("-----------------------------------------------------");
+
+            double result = 0;
+            double res = 0;
+            double r=0;
 
             while (sc.hasNext()) {
 
@@ -24,10 +28,12 @@ public class Main {
                 sc.hasNextInt();
                 int a = sc.nextInt();
                 System.out.print(String.format("%5d%2s", a, "="));
-                double result = d * a;
+                result = d * a;
                 System.out.print(String.format("%10.2f%n", result));
+                 res=res+result;
             }
             System.out.println("-----------------------------------------------------");
+            System.out.println(String.format("%-1s%23s%10.2f","Итого","=", res));
         } catch (IOException e) {
             e.printStackTrace();
 
